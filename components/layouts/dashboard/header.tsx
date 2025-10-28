7c109f2b8f9659d2e7efee9d0dc9df460217ba96
Badejo Adebola <adebolabadejo11@gmail.com>
2025-10-28 22:38:30 +0100

    incomplete header

--- components/layouts/dashboard/header.tsx
+++ components/layouts/dashboard/header.tsx
@@ -1,11 +1,54 @@
-import React from 'react'
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupInput,
+} from "@/components/ui/input-group";
+import { SearchIcon } from "lucide-react";
+import { AiOutlineMenuUnfold, AiOutlineMenuFold } from "react-icons/ai";
+import { IoNotifications } from "react-icons/io5";
+import { Badge } from "@/components/ui/badge";
+import React from "react";
 
-const DashboardHeader = () => {
+export default function DashboardHeader({
+  sidebarOpen,
+  setSidebarOpen,
+  darkMode,
+  setDarkMode,
+}) {
   return (
-    <div>
-        
-    </div>
-  )
+    <header
+      className={`sticky top-0 z-30 ${
+        darkMode ? "bg-gray-800 border-gray-700" : "bg-white border-gray-200"
+      } border-b`}
+    >
+      <div className="flex items-center justify-between h-16 px-6">
+        <div className="flex items-center space-x-4">
+          <button
+            onClick={() => setSidebarOpen(!sidebarOpen)}
+            className="text-xl cursor-pointer"
+          >
+            {sidebarOpen ? <AiOutlineMenuUnfold /> : <AiOutlineMenuFold />}
+          </button>
+          <InputGroup className="rounded-full">
+            <InputGroupInput placeholder="Search" />
+            <InputGroupAddon>
+              <SearchIcon />
+            </InputGroupAddon>
+          </InputGroup>
+        </div>
+        <div className="flex items-center justify-between h-16 px-6">
+          <div className="relative">
+            <Badge
+              className="h-4 min-w-3 rounded-full px-1 font-mono tabular-nums text-sm absolute -mt-2 ml-2"
+              variant="destructive"
+            >
+              6
+            </Badge>
+            <IoNotifications className="text-2xl text-[#3D42DF]"/>
+          </div>
+          
+        </div>
+      </div>
+    </header>
+  );
 }
-
-export default DashboardHeader
