0ca1f37d57d594abee494e57a013ff8925329353
Badejo Adebola <adebolabadejo11@gmail.com>
2025-10-31 23:16:56 +0100

    dashboard navigation tab added

--- components/layouts/dashboard/sidebar.tsx
+++ components/layouts/dashboard/sidebar.tsx
@@ -1,5 +1,36 @@
 import React from "react";
 import DashbordTab from "./tab";
+import { RiDashboard3Line } from "react-icons/ri";
+import { MdFavoriteBorder } from "react-icons/md";
+import { TiMessages } from "react-icons/ti";
+import { TbListCheck } from "react-icons/tb";
+import { LiaBorderAllSolid } from "react-icons/lia";
+import { LuRows3 } from "react-icons/lu";
+import { Separator } from "@/components/ui/separator";
+import { CiGift } from "react-icons/ci";
+import { IoCalendarOutline } from "react-icons/io5";
+
+
+
+const tabOne = [
+  { title: "Dashboard", icon: <RiDashboard3Line /> },
+  { title: "Products", icon: <LiaBorderAllSolid /> },
+  { title: "favorites", icon: <MdFavoriteBorder /> },
+  { title: "inbox", icon: <TiMessages /> },
+  { title: "Order Lists", icon: <TbListCheck /> },
+  { title: "Product Stock", icon: <LuRows3 /> },
+];
+
+const tabTwo = [
+  { title: "Pricing", icon: <CiGift /> },
+  { title: "Calendar", icon: <IoCalendarOutline /> },
+  { title: "To-Do", icon: <MdFavoriteBorder /> },
+  { title: "Contact", icon: <TiMessages /> },
+  { title: "Invoice", icon: <TbListCheck /> },
+  { title: "UI Elements", icon: <LuRows3 /> },
+  { title: "Team", icon: <LuRows3 /> },
+  { title: "Table", icon: <LuRows3 /> },
+];
 
 export default function DashboardSidebar({
   sidebarOpen,
@@ -10,19 +41,38 @@ export default function DashboardSidebar({
 }) {
   return (
     <aside
-      className={`fixed top-0 z-40 h-screen transition-transform duration-300 ${
+      className={`fixed top-0 z-40 h-screen transition-transform duration-300 overflow-y-auto ${
         sidebarOpen ? "transition-x-0" : "-transition-x-full"
-      } ${darkMode ? 'bg-gray-800 border-gray-700' : 'bg-white boarder-gray-200'} border-r`}
-       style={{width: '250px'}}
+      } ${
+        darkMode ? "bg-gray-800 border-gray-700" : "bg-white boarder-gray-200"
+      } border-r`}
+      style={{ width: "250px" }}
     >
       <div className="flex items-center justify-center h-16 w-full border-gray-200 dark:border-gray-700">
         <div className="flex items-center">
-            <span className="text-[#4880FF] max-md:text-lg font-bold text-2xl">Dash</span>
-          <span className={`font-bold text-2xl max-md:text-lg ${darkMode ? 'text-white' : 'text-black'}`}>Stack</span>
+          <span className="text-[#4880FF] max-md:text-lg font-bold text-2xl">
+            Dash
+          </span>
+          <span
+            className={`font-bold text-2xl max-md:text-lg ${
+              darkMode ? "text-white" : "text-black"
+            }`}
+          >
+            Stack
+          </span>
         </div>
       </div>
-      <div className="pt-4">
-          <DashbordTab/>
+      <div className="pt-4 space-y-1">
+        {tabOne.map((tab, index) => (
+          <DashbordTab key={index} title={tab.title} icon={tab.icon} />
+        ))}
+      </div>
+      <Separator className="my-4 text-[#D8D8D8]" />
+      <p className="text-gray-500 font-semibold text-sm text-start">PAGES</p>
+      <div className="pt-4 space-y-1">
+        {tabTwo.map((tab, index) => (
+          <DashbordTab key={index} title={tab.title} icon={tab.icon} />
+        ))}
       </div>
     </aside>
   );
--- components/layouts/dashboard/tab.tsx
+++ components/layouts/dashboard/tab.tsx
@@ -1,15 +1,33 @@
+import { Button } from "@/components/ui/button";
+import { ReactNode } from "react";
 import { IconType } from "react-icons/lib";
 
 interface props {
-    icon: IconType
-    title: string
+  icon: ReactNode;
+  title: string;
 }
 
-export default function DashbordTab ({icon, title}: props) {
-    return (
-        <button className="w-full ">
-            
-        </button>
-    )
+export default function DashbordTab({ icon, title }: props) {
+  return (
+    <div className="w-full flex justify-center relative group group-active:">
+      <span
+        className="h-10 w-12 absolute -ml-72 rounded-lg 
+        group-hover:bg-[#4880FF] 
+        group-hover:text-white
+        group-active:bg-[#4880FF] 
+        group-active:text-white"
+      ></span>
 
+      <Button
+        className="h-10 w-3/4 bg-white text-black text-sm justify-start
+        hover:bg-[#4880FF]
+        group-hover:bg-[#4880FF]
+        group-hover:text-white
+        group-active:bg-[#4880FF] 
+        group-active:text-white"
+      >
+        {icon} {title}
+      </Button>
+    </div>
+  );
 }
