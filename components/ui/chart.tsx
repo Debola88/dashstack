94d9c6b532afe4b73d3017a30a13a171348de8cd
Badejo Adebola <adebolabadejo11@gmail.com>
2025-11-07 12:17:04 +0100

    Minor edits

--- components/ui/chart.tsx
+++ components/ui/chart.tsx
@@ -180,8 +180,8 @@ function ChartTooltipContent({
       {!nestLabel ? tooltipLabel : null}
       <div className="grid gap-1.5">
         {payload
-          .filter((item:any) => item.type !== "none")
-          .map((item:any, index: any) => {
+          .filter((item) => item.type !== "none")
+          .map((item, index) => {
             const key = `${nameKey || item.name || item.dataKey || "value"}`
             const itemConfig = getPayloadConfigFromPayload(config, item, key)
             const indicatorColor = color || item.payload.fill || item.color
