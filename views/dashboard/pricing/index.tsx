b7b0dce93a397e4159252fc47bca070a9193dab9
Badejo Adebola <adebolabadejo11@gmail.com>
2026-01-12 23:35:12 +0100

    minor update

--- views/dashboard/pricing/index.tsx
+++ views/dashboard/pricing/index.tsx
@@ -1,8 +1,11 @@
+import PriceCard from "./_component/price-card";
+
 export default function DashboardPricingView() {
   return (
     <div>
       <div className="flex justify-between max-sm:flex-col mb-4">
-        <h1 className="font-semibold text-2xl lg:px-3 mb-2">Product Stock</h1>
+        <h1 className="font-semibold text-2xl lg:px-3 mb-2">Pricing</h1>
+        <PriceCard />
       </div>
     </div>
   );
