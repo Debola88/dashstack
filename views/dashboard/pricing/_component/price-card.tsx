4d9f2a576c09aa0316951e94f9db85e60fc333c3
Badejo Adebola <adebolabadejo11@gmail.com>
2026-01-17 23:50:01 +0100

    minor edits

--- views/dashboard/pricing/_component/price-card.tsx
+++ views/dashboard/pricing/_component/price-card.tsx
@@ -2,7 +2,7 @@
 
 export default function PriceCard () {
     return (
-        <div>
+        <div className="Card">
             
         </div>
     )
--- views/dashboard/pricing/index.tsx
+++ views/dashboard/pricing/index.tsx
@@ -3,7 +3,7 @@ import PriceCard from "./_component/price-card";
 export default function DashboardPricingView() {
   return (
     <div>
-      <div className="flex justify-between max-sm:flex-col mb-4">
+      <div className=" justify-between max-sm:flex-col mb-4">
         <h1 className="font-semibold text-2xl lg:px-3 mb-2">Pricing</h1>
         <PriceCard />
       </div>
