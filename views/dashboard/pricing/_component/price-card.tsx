d5b85e52f1dcf0672242cb842d2498a2dea120ad
Badejo Adebola <adebolabadejo11@gmail.com>
2026-01-17 23:56:28 +0100

    card update in pricing page

--- views/dashboard/pricing/_component/price-card.tsx
+++ views/dashboard/pricing/_component/price-card.tsx
@@ -1,9 +1,18 @@
+import { Card, CardContent, CardDescription, CardHeader } from "@/components/ui/card";
 
 
 export default function PriceCard () {
     return (
-        <div className="Card">
-            
-        </div>
+        <Card className="">
+            <CardHeader className="">
+
+            </CardHeader>
+                            <CardDescription>
+                    
+                </CardDescription>
+            <CardContent>
+
+            </CardContent>
+        </Card>
     )
 }
