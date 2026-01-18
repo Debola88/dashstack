c2f5190ca0a1c08e9eb24395e30b66bb4a78894c
Badejo Adebola <adebolabadejo11@gmail.com>
2026-01-18 01:29:14 +0100

    [addition]-pricing card

--- views/dashboard/pricing/_component/price-card.tsx
+++ views/dashboard/pricing/_component/price-card.tsx
@@ -1,18 +1,29 @@
-import { Card, CardContent, CardDescription, CardHeader } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+} from "@/components/ui/card";
 
+interface pricingCard {
+  planName: "Basic" | "Standard" | "Premium";
+  color: string;
+  price: string;
+}
 
-export default function PriceCard () {
-    return (
-        <Card className="">
-            <CardHeader className="">
-
-            </CardHeader>
-                            <CardDescription>
-                    
-                </CardDescription>
-            <CardContent>
-
-            </CardContent>
-        </Card>
-    )
+export default function PriceCard({ planName, color, price }: pricingCard) {
+  return (
+    <Card className="w-full">
+      <CardHeader className="">{planName}</CardHeader>
+      <CardDescription>
+        <p>Monthly Charge</p>
+      </CardDescription>
+      <CardContent>
+        <p className="text-[#4880FF] text-3xl font-bold">
+          ${/* {price} */}
+          14.99
+        </p>
+      </CardContent>
+    </Card>
+  );
 }
--- views/dashboard/pricing/index.tsx
+++ views/dashboard/pricing/index.tsx
@@ -5,7 +5,11 @@ export default function DashboardPricingView() {
     <div>
       <div className=" justify-between max-sm:flex-col mb-4">
         <h1 className="font-semibold text-2xl lg:px-3 mb-2">Pricing</h1>
-        <PriceCard />
+        <div className="flex gap-4 md:flex-row">
+          <PriceCard />
+          <PriceCard />
+          <PriceCard />
+        </div>
       </div>
     </div>
   );
