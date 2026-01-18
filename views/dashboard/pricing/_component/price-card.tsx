2e08c3b146629ca802ebeaeb8926eaadbed10cf2
Badejo Adebola <adebolabadejo11@gmail.com>
2026-01-19 00:55:58 +0100

    Complete price card

--- views/dashboard/pricing/_component/price-card.tsx
+++ views/dashboard/pricing/_component/price-card.tsx
@@ -3,40 +3,113 @@ import {
   Card,
   CardContent,
   CardDescription,
+  CardFooter,
   CardHeader,
 } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
+import Link from "next/link";
+import React from "react"
 
-interface pricingCard {
-  planName: "Basic" | "Standard" | "Premium";
-  color: string;
-  price: string;
-}
 
-export default function PriceCard({ planName, color, price }: pricingCard) {
+export default function PriceCard() {
   return (
-    <Card className="w-full">
-      <CardHeader className="">{planName}</CardHeader>
-      <CardDescription>
-        <p>Monthly Charge</p>
-      </CardDescription>
-      <CardContent>
-        <p className="text-[#4880FF] text-3xl font-bold">
-          ${/* {price} */}
-          14.99
-        </p>
-        <ul>
-          <li>Free Setup</li>
-          <li>Bandwidth Limit 10GB</li>
-          <li>20 User Connection</li>
-          <li>Analytic Report</li>
-          <li>Public API Access</li>
-          <li>Plugins Integration</li>
-          <li>Custom Content management</li>
-        </ul>
-        <Separator />
-        <Button>Get Started</Button>
-      </CardContent>
-    </Card>
+    <React.Fragment>
+      <Card className="w-full text-center">
+        <CardHeader className="text-black font-semibold text-lg">
+          Basic
+        </CardHeader>
+        <CardDescription className="-mt-6">
+          <p>Monthly Charge</p>
+        </CardDescription>
+        <CardContent>
+          <p className="text-[#4880FF] text-3xl font-bold -mt-5">
+            $14.99
+          </p>
+          <Separator className="my-10 border" />
+          <ul className="space-y-3 text-sm font-semibold">
+            <li>Free Setup</li>
+            <li>Bandwidth Limit 10GB</li>
+            <li>20 User Connection</li>
+            <li className="text-[#212121]/40">Analytic Report</li>
+            <li className="text-[#212121]/40">Public API Access</li>
+            <li className="text-[#212121]/40">Plugins Integration</li>
+            <li className="text-[#212121]/40">Custom Content management</li>
+          </ul>
+          <Separator className="my-10 border" />
+          <Button className="rounded-full bg-white text-[#4880FF] border-2 border-[#4880FF] p-5 hover:bg-[#4880FF] hover:text-white">
+            Get Started
+          </Button>
+          <CardFooter className="flex justify-center">
+            <Link href="#" className="underline pt-4 text-sm">
+              Start your 30 Days free trial
+            </Link>
+          </CardFooter>
+        </CardContent>
+      </Card>
+       <Card className="w-full text-center">
+        <CardHeader className="text-black font-semibold text-lg">
+          Standard
+        </CardHeader>
+        <CardDescription className="-mt-6">
+          <p>Monthly Charge</p>
+        </CardDescription>
+        <CardContent>
+          <p className="text-[#4880FF] text-3xl font-bold -mt-5">
+            $49.99
+          </p>
+          <Separator className="my-10 border" />
+          <ul className="space-y-3 text-sm font-semibold">
+            <li>Free Setup</li>
+            <li>Bandwidth Limit 10GB</li>
+            <li>20 User Connection</li>
+            <li>Analytic Report</li>
+            <li>Public API Access</li>
+            <li className="text-[#212121]/40">Plugins Integration</li>
+            <li className="text-[#212121]/40">Custom Content management</li>
+          </ul>
+          <Separator className="my-10 border" />
+          <Button className="rounded-full bg-white text-[#4880FF] border-2 border-[#4880FF] p-5 hover:bg-[#4880FF] hover:text-white">
+            Get Started
+          </Button>
+          <CardFooter className="flex justify-center">
+            <Link href="#" className="underline pt-4 text-sm">
+              Start your 30 Days free trial
+            </Link>
+          </CardFooter>
+        </CardContent>
+      </Card> 
+      <Card className="w-full text-center">
+        <CardHeader className="text-black font-semibold text-lg">
+          Premium
+        </CardHeader>
+        <CardDescription className="-mt-6">
+          <p>Monthly Charge</p>
+        </CardDescription>
+        <CardContent>
+          <p className="text-[#4880FF] text-3xl font-bold -mt-5">
+           $89.99
+          </p>
+          <Separator className="my-10 border" />
+          <ul className="space-y-3 text-sm font-semibold">
+            <li>Free Setup</li>
+            <li>Bandwidth Limit 10GB</li>
+            <li>20 User Connection</li>
+            <li>Analytic Report</li>
+            <li>Public API Access</li>
+            <li>Plugins Integration</li>
+            <li>Custom Content management</li>
+          </ul>
+          <Separator className="my-10 border" />
+          <Button className="rounded-full bg-white text-[#4880FF] border-2 border-[#4880FF] p-5 hover:bg-[#4880FF] hover:text-white">
+            Get Started
+          </Button>
+          <CardFooter className="flex justify-center">
+            <Link href="#" className="underline pt-4 text-sm">
+              Start your 30 Days free trial
+            </Link>
+          </CardFooter>
+        </CardContent>
+      </Card>
+    </React.Fragment>
   );
 }
--- views/dashboard/pricing/index.tsx
+++ views/dashboard/pricing/index.tsx
@@ -5,10 +5,10 @@ export default function DashboardPricingView() {
     <div>
       <div className=" justify-between max-sm:flex-col mb-4">
         <h1 className="font-semibold text-2xl lg:px-3 mb-2">Pricing</h1>
-        <div className="flex gap-4 md:flex-row">
+        <div className="flex space-x-10 space-y-4 max-md:flex-col pt-5">
           <PriceCard />
-          <PriceCard />
-          <PriceCard />
+          {/* <PriceCard />
+          <PriceCard /> */}
         </div>
       </div>
     </div>
