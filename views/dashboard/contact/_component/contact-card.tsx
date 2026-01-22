ffadc35d985688cc569c26c28d44c1fd90a7213e
Badejo Adebola <adebolabadejo11@gmail.com>
2026-01-22 23:55:27 +0100

    minor additions

--- views/dashboard/contact/_component/contact-card.tsx
+++ views/dashboard/contact/_component/contact-card.tsx
@@ -1,13 +1,19 @@
 import { Card, CardHeader } from "@/components/ui/card";
 import Image from "next/image";
-import
+import placeImage from "@/assets/images/Bitmap (8).png";
 
 export default function ContactCard() {
   return (
-    <Card>
-      <CardHeader>
-        <Image src="" alt="" width={} height={} />
-      </CardHeader>
+    <Card className="relative">
+      <div className=" top-0">
+        <Image
+          src={placeImage}
+          alt=""
+          width={250}
+          height={250}
+          className="w-full"
+        />
+      </div>
     </Card>
   );
 }
