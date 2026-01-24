7e0d021f53757806440d2c89ed1f837970e46e45
Badejo Adebola <adebolabadejo11@gmail.com>
2026-01-24 14:49:45 +0100

    mirror-dev

--- views/dashboard/contact/_component/contact-card.tsx
+++ views/dashboard/contact/_component/contact-card.tsx
@@ -1,6 +1,8 @@
 import { Card, CardHeader } from "@/components/ui/card";
 import Image from "next/image";
 import placeImage from "@/assets/images/Bitmap (8).png";
+import { FaMessage } from "react-icons/fa6";
+import { Button } from "@/components/ui/button";
 
 export default function ContactCard() {
   return (
@@ -13,6 +15,13 @@ export default function ContactCard() {
           height={250}
           className="w-full"
         />
+      </div>
+      <div className="flex flex-col items-center">
+        <p className="font-semibold">Jason Price</p>
+        <p>kuhlman.jermey@yahoo.com</p>
+        <Button variant="outline" size="sm">
+          <FaMessage /> Message
+        </Button>
       </div>
     </Card>
   );
